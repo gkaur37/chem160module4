cd7e2606489dcff2b615888fc5c8a6849bfcc1c7
gkaur37 <54698652+gkaur37@users.noreply.github.com>
2019-09-16 23:06:38 -0700

    Add files via upload

---
+++ diffuse.py
@@ -0,0 +1,41 @@
+Python 3.7.4 (v3.7.4:e09359112e, Jul  8 2019, 14:54:52) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello world")
+Hello world
+>>> 7*1.3
+9.1
+>>> for i in range (1,11)
+SyntaxError: invalid syntax
+>>> for i in range (1,11):
+	y = i*i
+	print y
+	
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(y)?
+>>> for i in range (1,11):
+	y = i*i
+	print (y)
+
+	
+1
+4
+9
+16
+25
+36
+49
+64
+81
+100
+>>> 
+== RESTART: /Users/gursimrankaur/Downloads/chem160module4-master/diffuse.py ==
+side=101 <t>=3084.76  <t>/r2= 0.30
+>>> 
+=== RESTART: /Users/gursimrankaur/Downloads/chem160module4-master/ising.py ===
+<E>=-4.00
+<E>=0.25
+u u u d d u u d u d u d d u u d d d u d u d u u d d u d d u d u d d u u u d u d u u d d d u u d d u d d d u d d u d d d u u d u 
+>>> 
+===== RESTART: /Users/gursimrankaur/Desktop/chem160module4/isingsim2.py =====
+  4.0000  -1.120156   0.001733
+>>> 
